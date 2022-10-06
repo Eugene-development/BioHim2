193940d501fa3084d1483c0a4cc6e7b6222aa542
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-06 17:34:02 +0300

    c

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -462,7 +462,7 @@
 
 												{#if $selectBrand}
 													<ul
-														class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+														class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm border-2 border-cyan-500"
 														tabindex="-1"
 														role="listbox"
 														aria-labelledby="listbox-label"
@@ -537,7 +537,7 @@
 
 												{#if $selectContainer}
 													<ul
-														class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+														class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm border-2 border-cyan-500"
 														tabindex="-1"
 														role="listbox"
 														aria-labelledby="listbox-label"
